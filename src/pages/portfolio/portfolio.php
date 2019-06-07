6f978fd469ae97f3c5e8d48c166175d07a052c6b
Alae Es-saki <alaeessaki@gmail.com>
2019-06-07 12:23:35 +0000

    adding favicon to other pages

--- src/pages/portfolio/portfolio.php
+++ src/pages/portfolio/portfolio.php
@@ -16,6 +16,7 @@ if(isset($_GET['lang'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="../../favicon.ico?v=2" type="image/x-icon">
     <!--Bootstrap-->
     <link rel="stylesheet" href="../../lib/bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <!-- font awsome -->
